8d5a29dccce0f01eeec2a4d3ed249a970f9ec7b3
Nils <sterz.nils@protonmail.com>
2019-11-14 15:09:48 +0100

    switched back to xmonad + xmobar

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -1,27 +1,54 @@
 import XMonad hiding( (|||) )
+
+-- UTIL
 import XMonad.Util.EZConfig
 import XMonad.Util.Scratchpad
 import XMonad.Util.NamedScratchpad
-import XMonad.Util.Run(spawnPipe)
+import XMonad.Util.Run(spawnPipe, safeSpawn)
+import XMonad.Util.NamedWindows (getName)
+
+-- HOOKS
 import XMonad.Hooks.ManageDocks
 import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.ManageHelpers
 import XMonad.Hooks.SetWMName
+
+-- LAYOUT
 import XMonad.Layout.LayoutCombinators
 import XMonad.Layout.LayoutModifier
+
+-- MISC
+import Data.List (sortBy)
+import Data.Function (on)
+import Control.Monad (forM_, join)
 import qualified Data.Map as M
 import qualified XMonad.StackSet as W
-    
+
+{-
+myStartupHook = do
+  spawn "$HOME/.config/polybar/launch.sh"
+-}
+
+-- Some Colors
+orange = "ffcc00"
+black  = "000000"
+white  = "ffffff"
+blue   = "0033cc"
+red    = "ff0000"
+
+-- dark colors
+dblue  = "000066"
+
 
 myTerminal = "termite"
 myModmask = mod4Mask  -- win-key
 myBW = 2
-myFBC = "orange"
---myFBC = "#ff0000"
+myFBC = orange
 
-myXmobarrc = "~/.xmonad/xmobarrc.hs"
 
--- hide xmobar with mod+b. keycodes 
+myXmobarrc = "~/.xmonad/xmobarrc"
+
+-- hide xmobar with mod+b. keycodes
 toggleStrutsKey XConfig {XMonad.modMask = modMask} = (modMask, xK_b)
 
 -- Key-codes can be found in: '/usr/include/X11/keysymdef.h'
@@ -30,45 +57,56 @@ myKeys = [
   ((mod4Mask, xK_u), spawn "$HOME/xmonad-update.sh"),
   ((0, xK_Print), spawn "scrot"),
 
+  -- Application Launcher
+  ((mod4Mask, xK_space), spawn "rofi -show drun -display-drun :"),
+
   -- Window movement
   ((mod4Mask, xK_Up), windows W.swapUp),
   ((mod4Mask, xK_f), sendMessage $ JumpToLayout "Full"),
-  ((mod4Mask, xK_s), sendMessage $ JumpToLayout "Tall"), 
-                                        
+  ((mod4Mask, xK_s), sendMessage $ JumpToLayout "Tall"),
+
   -- MUSIC RELATED
   ((0, xK_Pause), spawn "mpc toggle"),
   ((mod4Mask, xK_Page_Up), spawn "mpc prev"),
-  ((mod4Mask, xK_Page_Down), spawn "mpc next") 
+  ((mod4Mask, xK_Page_Down), spawn "mpc next")
   ]
-
-myPP = xmobarPP {ppCurrent = xmobarColor "#ff0000" "" . wrap "{" "}"}
 
 myManageHooks = composeAll [
-  className =? "Qutebrowser"		--> doShift "1:WWW",
-  className =? "mutt"        		--> doShift "8:MAIL",
-  className =? "Emacs"       		--> doShift "3:TeX",
-  className =? "XDvi"        		--> doCenterFloat,
-  className =? "Xfig"        		--> doShift "5:PLOT",
-  className =? "Zathura"     		--> doCenterFloat,
-  className =? "Nm-connection-editor" 	--> doCenterFloat,
-  className =? "Gnuplot"		--> doCenterFloat,
-  resource  =? "music"       		--> doShift "7:MUSIC",
-  isFullscreen		     		--> (doF W.focusDown <+> doFullFloat)
+  className =? "Qutebrowser"            --> doShift "1:WWW",
+  className =? "mutt"                   --> doShift "8:MAIL",
+  className =? "Emacs"                  --> doShift "3:TeX",
+  className =? "XDvi"                   --> doCenterFloat,
+  className =? "Xfig"                   --> doShift "5:PLOT",
+  className =? "Zathura"                --> doCenterFloat,
+  className =? "Nm-connection-editor"   --> doCenterFloat,
+  className =? "Gnuplot"                --> doCenterFloat,
+
+  -- Steam
+  className =? "Friends List"           --> doCenterFloat,
+  className =? "Steam News"             --> doCenterFloat,
+  className =? "Settings"               --> doCenterFloat,
+
+  resource  =? "music"                  --> doShift "7:MUSIC",
+  isFullscreen                          --> (doF W.focusDown <+> doFullFloat)
   ]
 
-myWS = ["1:WWW", "2:UEB", "3:TeX", "4:PDF", "5:PLOT", "6:MISC", "7:MUSIC", "8:MAIL"]
+myWS = ["1:WWW", "2:UEB", "3:TeX", "4:PDF", "5:MAIL", "6:MISC", "7:MUSIC", "8:GAMES"]
 
 tall = Tall 1 (3/100) (1/2)
-       
-myConfig = defaultConfig{
+
+
+myConfig = defaultConfig {
    terminal = myTerminal,
    modMask = myModmask,
    borderWidth = myBW,
    workspaces = myWS,
    focusedBorderColor = myFBC,
    manageHook = myManageHooks,
+   -- startupHook = myStartupHook,
    layoutHook = tall ||| Mirror tall ||| Full
    } `additionalKeys` myKeys
-     
-main = xmonad =<< statusBar "xmobar" myPP toggleStrutsKey myConfig
 
+myPP = xmobarPP {ppCurrent = xmobarColor "#ff0000" "" . wrap "{" "}"}
+
+
+main = xmonad =<< statusBar "xmobar" myPP toggleStrutsKey myConfig
